a18050f154775f839061ce2669875f8203f99f37
Azhar Nugraha <57381268+azhargraha@users.noreply.github.com>
2022-08-15 20:09:21 +0700

    add final templating

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,6 +3,14 @@ import styles from '../styles/Hero.module.css';
 
 export const Hero = () => {
   return (
-    <div>Hero</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.heading}>
+        <h1>Hero element</h1>
+        <a href="https://dribbble.com/shots/15955423-Yum" target="_blank" rel="noopener noreferrer">Source image</a>
+      </div>
+      <picture className>
+        <img src="https://cdn.dribbble.com/users/3320958/screenshots/15955423/media/ec2fe461084339c3a4d14efae1202847.jpeg?" alt="Hero element" />
+      </picture>
+    </div>
+  );
 };
