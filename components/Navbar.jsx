6871014572bdf82b17c64a13714d3259e6d1048e
Azhar Nugraha <57381268+azhargraha@users.noreply.github.com>
2022-08-15 20:09:28 +0700

    add final templating

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,17 +1,66 @@
-import React from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import Image from 'next/image';
+React.useLayoutEffect = React.useEffect;
 import styles from '../styles/Navbar.module.css';
 
+import hamburgerOpen from '../public/assets/hamburger-open.svg';
+import hamburgerClose from '../public/assets/hamburger-close.svg';
+
+const useWindowSize = () => {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+};
+
 export const Navbar = () => {
+  const [width] = useWindowSize();
+  const [isMobile, setIsMobile] = useState(width <= 590);
+  const [isToggle, setIsToggle] = useState(true);
+
+  const handleToggle = (e) => {
+    e.preventDefault();
+    setIsToggle(!isToggle);
+  };
+
+  useEffect(() => {
+    if (width > 590) {
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+    }
+  }, [width]);
+
   return (
     <nav className={styles.container}>
       <div className={styles.logo}>
-        <h1>Hunger Apps</h1>
+        <a href="#">Hunger Apps</a>
       </div>
-      <ul className={styles.menus}>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Favorite</a></li>
-        <li><a href="#">About Us</a></li>
-      </ul>
+      {
+        isMobile ? 
+          <button className={styles.toggle} onClick={handleToggle}>
+            <Image src={isToggle ? hamburgerClose : hamburgerOpen} alt="test" />
+          </button> :
+          <ul className={styles.menus_close}>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Favorite</a></li>
+            <li><a href="#">About Us</a></li>
+          </ul>
+      }
+      {
+        (isToggle && isMobile) &&
+          <ul className={styles.menus_open}>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Favorite</a></li>
+            <li><a href="#">About Us</a></li>
+          </ul>
+      }
     </nav>
-  )
-}
+  );
+};
