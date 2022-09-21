fc5b657949c403a2101f647926350c677bf2f92e
Azhar Nugraha <57381268+azhargraha@users.noreply.github.com>
2022-09-22 00:59:03 +0700

    tugas react hooks

--- pages/favorite/index.jsx
+++ pages/favorite/index.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { restaurants } from '../../restaurantData.json';
 import styles from '../../styles/Favorite.module.css';
 import { Card } from '../../components/Card';
 
 const Favorite = () => {
+  const [count, setCount] = useState(1);
+
   return (
     <div>
       <section className={styles.container}>
-        <h2>Restaurant Menus</h2>
+        <header className={styles.header}>
+          <h2>Restaurant Menus</h2>
+          <div className={styles.counter}>
+            <button className={styles.minus} onClick={() => setCount(count - 1)} disabled={count <= 1} />
+            <h5>{count}</h5>
+            <button className={styles.plus} onClick={() => setCount(count + 1)} />
+          </div>
+        </header>
         <div className={styles.restaurants}>
-          {
-            restaurants.map((data, i) => (
-              <Card key={i} id={data.id} rating={data.rating} img={data.img} menus={data.menus} title={data.title} />
-            ))
-          }
+          {restaurants.map((data, i) => (
+            <Card key={i} id={data.id} rating={data.rating} img={data.img} menus={data.menus} title={data.title} />
+          ))}
         </div>
       </section>
     </div>
