c515cb3de4b50c0fe2339f5f02d333dce335ca6a
Azhar Nugraha <57381268+azhargraha@users.noreply.github.com>
2022-09-28 23:44:08 +0700

    connected to mongodb

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,15 @@
 import NextAuth from "next-auth";
 import GithubProvider from 'next-auth/providers/github';
+import clientPromise from "../../../lib/mongodb";
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 
 export default NextAuth({
+    adapter: MongoDBAdapter(clientPromise),
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
         })
     ],
-    secret: process.env.NEXTAUTH_SECRET
+    secret: process.env.NEXTAUTH_SECRET,
 });
